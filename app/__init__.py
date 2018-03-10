6f2de1ca4f8bc1b692af166aa4978117318f0173
saber <635344988@qq.com>
2018-03-10 11:20:39 +0800

    Initial commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flasky import Flask, render_template
+from flask import Flask, render_template
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.mail import Mail
 from flask.ext.moment import Moment
