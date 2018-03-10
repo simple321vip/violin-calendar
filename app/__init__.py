3fc5c0cc82ff2e6692c1a1a236a4f0720bb32fa5
saber <635344988@qq.com>
2018-03-10 23:58:46 +0800

    Initial commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.mail import Mail
 from flask.ext.moment import Moment
@@ -10,14 +10,20 @@ mail = Mail()
 moment = Moment()
 db = SQLAlchemy()
 
+
 def create_app(config_name):
     app = Flask(__name__)
+    print(__name__)
     app.config.from_object(config[config_name])
+    print(config_name)
     config[config_name].init_app(app)
 
     bootStrap.init_app(app)
     mail.init_app(app)
     moment.init_app(app)
     db.init_app(app)
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
 
     return app
