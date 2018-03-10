b3478b546bc0328b7ae4f80ce57bdaf3c8312518
saber <635344988@qq.com>
2018-03-11 01:16:19 +0800

    Initial commit

--- app/__init__.py
+++ app/__init__.py
@@ -13,9 +13,7 @@ db = SQLAlchemy()
 
 def create_app(config_name):
     app = Flask(__name__)
-    print(__name__)
     app.config.from_object(config[config_name])
-    print(config_name)
     config[config_name].init_app(app)
 
     bootStrap.init_app(app)
